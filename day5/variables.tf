1a497c166f0800de441b1f4ab0ef1fea34187429
rajendran-priya <raajendran.priya@gmail.com>
2026-01-02 14:41:03 -0800

    changed the bucket name

--- day5/variables.tf
+++ day5/variables.tf
@@ -7,6 +7,6 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "day5-terraform-oubt-training-v2"
+  default     = "day5-terraform-oubt-training-bucket"
 }
 #Trigger run 
