dc2d9b92d38bc579f1a18dc82d36fe824c3c0276
rajendran-priya <raajendran.priya@gmail.com>
2026-01-02 12:17:00 -0800

    add lambda function and s3 bucket notification trigger

--- day5/outputs.tf
+++ day5/outputs.tf
@@ -6,3 +6,7 @@ output "s3_bucket_arn" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.my_app_bucket.id
 }
+
+output "lambda_function_name" {
+  value = aws_lambda_function.s3_processor.function_name
+}
